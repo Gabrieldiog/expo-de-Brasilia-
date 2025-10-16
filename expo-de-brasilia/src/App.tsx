3ebd66ec89dfda59033ee0cb66a630cde0abec2b
Gabrieldiog <gdiogo.software@gmail.com>
2025-10-16 02:48:18 -0300

    transicao beta fotos ilustrativas pois ele vai ter que escolher quais fotos vai aoarecer ali

---
+++ expo-de-brasilia/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import BrasiliaTransitions from './components/BrasiliaTransitions';
+import './App.css';
+
+const App: React.FC = () => {
+  return <BrasiliaTransitions />;
+};
+
+export default App;
